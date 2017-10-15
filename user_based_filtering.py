c3727f3cd4c0038acabcba4141b215542016aca6
mianxiang <32014850+mianxiang@users.noreply.github.com>
2017-10-15 22:38:38 +0800

    Update user_based_filtering.py
    Improved the user based recommendation by calculate similarity value firstly, then it is not necessary to calculate the similar value every time.

--- user_based_filtering.py
+++ user_based_filtering.py
@@ -4,9 +4,11 @@ import datetime
 prefers = recommendations.loadMovieLens()
 
 startTime = datetime.datetime.now()
-print(recommendations.getRecommendations(prefers, "87")[0:30])
+similarityValue = recommendations.calculateSimilarityValue(prefers)
+print(recommendations.getRecommendationsImproved(prefers, "87", similarityValue)[0:30])
 print((datetime.datetime.now() - startTime).total_seconds())
 
 startTime = datetime.datetime.now()
-print(recommendations.getRecommendations(prefers, "88")[0:30])
+print(recommendations.getRecommendationsImproved(prefers, "88", similarityValue)[0:30])
 print((datetime.datetime.now() - startTime).total_seconds())
+
